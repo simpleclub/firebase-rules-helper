94339c788afdb872bf804e6f6778c54998034114
Mateusz Prusaczyk <mateusz.prusaczyk@simpleclub.com>
2023-07-07 16:05:55 +0200

    enable ESM in tests

--- firebase-rules-coverage/jest.config.ts
+++ firebase-rules-coverage/jest.config.ts
@@ -1,12 +1,23 @@
-module.exports = {
+import type {JestConfigWithTsJest} from 'ts-jest';
+
+// This configuration enables ESM
+// and is inspired by
+// https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+const config: JestConfigWithTsJest = {
   roots: ['<rootDir>/src/', '<rootDir>/test/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': ['ts-jest', {useESM: true}],
   },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts'],
   coverageReporters: ['text', 'text-summary', ['lcov', {projectRoot: '../'}]],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
 };
+
+export default config;
